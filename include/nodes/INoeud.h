e2ec4d8263fe72843e5b5cba934cbcca9cc958c2
WarperSan <leumas.ecole@gmail.com>
2026-04-08 16:28:10 -0400

    Added `INoeud::configurer()` and `INoeud::lier()`

--- include/nodes/INoeud.h
+++ include/nodes/INoeud.h
@@ -2,6 +2,7 @@
 #define INC_8INF334_TP3_INOEUD_H
 
 #include "../Contexte.h"
+#include "../../library/json.hpp"
 
 namespace nodes
 {
@@ -12,6 +13,16 @@ namespace nodes
 
 		/// Executes this node
 		[[nodiscard]] virtual INoeud* executer(Contexte& contexte) = 0;
+
+		/// Configures this node with the given parameters
+		virtual void configurer(const nlohmann::basic_json<>& json)
+		{
+		}
+
+		/// Links the related nodes to the loaded nodes
+		virtual void lier(const std::unordered_map<std::string, INoeud*>& noeuds)
+		{
+		}
 	};
 }
 
